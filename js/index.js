520ef193a4c1400b96522520f1da7ccea87fc570
forebjina <forebjina@daum.net>
2020-12-07 01:22:22 +0900

    [update files] html : add h4 tag(movieTit) ,  move class searchWrap (inside header tag), ... css : change background-color , ... js : make function reqAjax , ...

--- js/index.js
+++ js/index.js
@@ -1,12 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
-    let movieItemList = document.querySelector('#movieItemList');
     const MOVIE_OMDAPI_URL = 'https://www.omdbapi.com/?apikey=abf73aa0&s='; //인증키
+   
+    let movieItemList = document.querySelector('#movieItemList'); 
+    let searchWord ='';//API검색키워드
+    let apiUrl = '';
+    searchWord = 'man';
+    apiUrl = MOVIE_OMDAPI_URL + searchWord
     //페이지 로딩시 'man'이라는 키워드로 영화 리스트 요청
-    let xhr;
-    let searchWord ='';
-    let reqURL ='';
-    searchWord ='man'
-    //reqURL = MOVIE_OMDAPI_URL+searchWord;
+    reqAjax(apiUrl, movieItemList);
+
+    //검색버튼 클릭시
+    const searchVal = document.querySelector('#searchVal'); 
+    const searchBtn = document.querySelector('#searchBtn');
+    searchBtn.addEventListener('click',function(){
+        while (movieItemList.firstChild) {
+            movieItemList.removeChild(movieItemList.firstChild);
+         };
+        searchWord = searchVal.value;
+        apiUrl = MOVIE_OMDAPI_URL + searchWord;
+        reqAjax(apiUrl, movieItemList);
+    });
+});
+
+function reqAjax(url, movieItemList){
+    let xhr = '';
     if(window.XMLHttpRequest){
         xhr = new XMLHttpRequest();
     } else{
@@ -14,48 +31,22 @@ document.addEventListener('DOMContentLoaded', function(){
         xhr = new ActiveXObject('Microsofr.XMLHTTP');
     }
     xhr.onreadystatechange = function(){
-        if(xhr.readyState == 4 && xhr.status == 200){
-            addMovieItemlist(xhr,movieItemList);
-        } else{
-            alert('통신실패');
-        }
-    }
-    xhr.open('GET',MOVIE_OMDAPI_URL+searchWord,true); //open(method,url,async)
-    xhr.send();
-
-    //검색버튼 클릭시
-    let searchInp = document.querySelector('#searchInp');
-    let searchBtn = document.querySelector('#searchBtn');
-    searchBtn.addEventListener('click',function(){
-        while (movieItemList.firstChild)  {
-                    movieItemList.removeChild(movieItemList.firstChild);
-                };
-        searchWord = searchInp.value;
-        if(window.XMLHttpRequest){
-            xhr = new XMLHttpRequest();
-        } else{
-            //IE5, IE6일때
-            xhr = new ActiveXObject('Microsofr.XMLHTTP');
-        }
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState == 4 && xhr.status == 200){
+        if(xhr.readyState == 4 ){
+            if( xhr.status == 200){
                 addMovieItemlist(xhr,movieItemList);
             } else{
                 alert('통신실패');
             }
         }
-        xhr.open('GET',MOVIE_OMDAPI_URL+searchWord,true); //open(method,url,async)
-        xhr.send();    
-    
-    });
-});
+    }
+    xhr.open('GET', url, true); //open(method,url,async)
+    xhr.send();  
+}
 
 function addMovieItemlist(xhr,movieItemList){
     const strRes = xhr.responseText;
     const jsonRes = JSON.parse(strRes);
-    console.log(typeof(strRes));
-    console.log(typeof(jsonRes));
-    console.log(xhr.responseText);
+
     //Search객체배열사용
     //class 한개 받아오는 거 ..반복문뿐이 없나봄..?
     let movieItemCnt = jsonRes.Search.length; //받아온 Search객체배열 개수
@@ -67,15 +58,20 @@ function addMovieItemlist(xhr,movieItemList){
         let li = document.createElement('li');
         movieItemLi = movieItemList.appendChild(li);
         movieItemLi.classList.add('movieItem');
-        //li>img
+        //li>h2 영화제목
+        let h4 = document.createElement('h4');   
+        movieTit =  movieItemLi.appendChild(h4);
+        movieTit.classList.add('movieTit');
+        movieTit.innerHTML = jsonRes.Search[i].Title;
+        //li>img 영화포스터이미지
         let img = document.createElement('img');
         movieImg = movieItemLi.appendChild(img);
         movieImg.classList.add('movieImg');
         movieImg.setAttribute('src',jsonRes.Search[i].Poster);
-        //li>p
+        //li>p //영화개봉년도
         let p = document.createElement('p');   
         movieTit =  movieItemLi.appendChild(p);
-        movieTit.classList.add('movieTit');
-        movieTit.innerHTML = jsonRes.Search[i].Title;
+        movieTit.classList.add('movieYear');
+        movieTit.innerHTML = jsonRes.Search[i].Year;
     }
 }
